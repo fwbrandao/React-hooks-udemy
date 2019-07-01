27775d102260303fdd8195ed0fb8eff25e505216
Fernando Brandao <fwbrandao@gmail.com>
2019-07-01 08:15:25 +0100

    appFunction toggle

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,12 +2,29 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1);
     }
 
-    return <button onClick={incrementCount}>I was clicked {count} times</button>
+    const toggleLight = () => {
+        setIsOn(prevIsOn => !prevIsOn);
+    }
+
+    return <>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>I was clicked {count} times</button>
+            <h2>Toggle Light</h2>
+                <div
+                    onClick={toggleLight}
+                    style={{
+                        height: "50px",
+                        width: "50px",
+                        background: isOn ? "yellow" : "grey"
+                    }}>
+                </div>
+            </>
 };
 
 export default App;
