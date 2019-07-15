3182a35d183ec40cce5b6f98b0878c31c9204abf
Fernando Brandao <fwbrandao@gmail.com>
2019-07-15 08:15:29 +0100

    geolocation

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,23 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+    latitude: null,
+    longitude: null,
+    speed: null
+}
+
 const App = () => {
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
     const [mousePosition, setMousePosition] = useState({x: null, y: null});
     const [status, setStatus] = useState(navigator.onLine);
+    const [{latitude, longitude, speed}, setLocation] = useState(initialLocationState);
+
+    let mounted = true;
 
     useEffect(() => {
         document.title = `Clicked ${count} times`;
         window.addEventListener('mousemove', handleMouseMove);
         window.addEventListener('online', handleOnline);
         window.addEventListener('offline', handleOffline);
+        navigator.geolocation.getCurrentPosition(handleGeolocation);
+        const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
             window.removeEventListener('online', handleOnline);
             window.removeEventListener('offline', handleOffline);
+            navigator.geolocation.clearWatch(watchId);
+            mounted = false;
         }
-    }, [count])
+        }, [count]
+    );
+
+    const handleGeolocation = event => {
+        if(mounted) {
+            setLocation({
+            latitude: event.coords.latitude,
+            longitude: event.coords.longitude,
+            speed: event.coords.speed
+            })
+        }
+    }
 
     const handleOnline = () => {
         setStatus(true)
@@ -61,6 +85,11 @@ const App = () => {
 
             <h2>Network Status</h2>
             <p>You are <strong>{status ? "Online" : "Offline"}</strong></p>
+
+            <h2>Geolocation</h2>
+            <p>Latitude is {latitude}</p>
+            <p>Longitude is {longitude}</p>
+            <p>Speed is {speed ? speed : "0 km/h"}</p>
             </>
 };
 
