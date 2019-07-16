f7bf1fb2e331c0d35f3b5113d18d774aeaafcf67
Fernando Brandao <fwbrandao@gmail.com>
2019-07-16 08:37:03 +0100

    Register form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './AppClass';
 // import App from './AppFunction';
-import App from './Login';
+// import App from './Login';
+import App from './Register';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
